0f61f8ab6fc645e2a9e43d6bbe9e385f517f206a
unknown <janoncarlos@gmail.com>
2023-09-22 16:08:07 -0300

    svg page

---
+++ app/projects/page.tsx
@@ -0,0 +1,8 @@
+export default function ProjectPage() {
+  return (
+    <>
+      <h1>PROJECTS</h1>
+      <h3>check my works</h3>
+    </>
+  );
+}
---
+++ app/projects/svg/page.tsx
@@ -0,0 +1,8 @@
+export default function SvgPage() {
+  return (
+    <>
+      <h1>SVG</h1>
+      <h3>check my works</h3>
+    </>
+  );
+}
