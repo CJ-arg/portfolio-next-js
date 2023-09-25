b72995311fd8e89a25a373760a79e78860f60b74
unknown <janoncarlos@gmail.com>
2023-09-25 19:30:04 -0300

    various  pages

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,7 +2,7 @@ export default function ContactPage() {
   return (
     <>
       <h1>CONTACT</h1>
-      <h3>check my works</h3>
+      <h3>CONTACT FORM</h3>
     </>
   );
 }
--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -2,7 +2,7 @@ export default function ExperiencePage() {
   return (
     <>
       <h1>Experience</h1>
-      <h3>check my works</h3>
+      <h3>My works</h3>
     </>
   );
 }
