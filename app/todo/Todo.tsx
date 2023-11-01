62b07070ab3ffd8cef3f54afb1428508e019e8c7
unknown <janoncarlos@gmail.com>
2023-11-01 15:56:04 -0300

    prueba todo context ts reducer

--- app/todo/Todo.tsx
+++ app/todo/Todo.tsx
@@ -1,15 +1,17 @@
+"use client";
 import React from "react";
+import TodoProvider from "./context/TodoProvider";
 
 const Todo = () => {
   return (
-    <>
+    <TodoProvider>
       <h1>Todo:</h1>
       <ul>
         <li>TODO 1 </li>
         <li>TODO 2 </li>
         <li>TODO 3</li>
       </ul>
-    </>
+    </TodoProvider>
   );
 };
 
--- app/todo/context/TodoProvider.tsx
+++ app/todo/context/TodoProvider.tsx
@@ -1,8 +1,39 @@
-import React from "react";
+import React, { useReducer } from "react";
 import { TodoContext } from "./TodoContext";
+import { JsxElement } from "typescript";
+import { TodoState } from "../interfaces/interfaces";
+import { todoReducer } from "./todoReducer";
+import { initialize } from "next/dist/server/lib/render-server";
 
-const TodoProvider = () => {
-  return <TodoContext.Provider>{children}</TodoContext.Provider>;
+const INITIAL_STATE: TodoState = {
+  todoCount: 2,
+  todos: [
+    {
+      id: "1",
+      description: "Recolectar las piedras",
+      completed: false,
+    },
+    {
+      id: "2",
+      description: "Recolectar las maderas",
+      completed: false,
+    },
+  ],
+  completed: 0,
+  pending: 2,
+};
+
+interface TodoProviderProps {
+  children: JSX.Element | Jsx.Element[];
+}
+
+const TodoProvider = ({ children }: TodoProviderProps) => {
+  const [taskState, dispatch] = useReducer(todoReducer, INITIAL_STATE);
+  return (
+    <TodoContext.Provider value={{ taskState }}>
+      {children}
+    </TodoContext.Provider>
+  );
 };
 
 export default TodoProvider;
